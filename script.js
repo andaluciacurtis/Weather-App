164f9fab8c2c77b970b656f1664c7333f5cdaac3
andaluciacurtis <andalucia.curtis@gmail.com>
2023-12-07 18:36:44 -0800

    linked css reset and js file, added psueodocode

---
+++ script.js
@@ -0,0 +1,7 @@
+// Allow the user to search for a city and list cities that come up for them to choose from
+// Once a city is chosen, send the coordinates to the weather API and display
+// Also display the name along with the weather
+
+// Weather necessities:
+// - current weather
+// - weather in an hour, two, three, etc, up to 10
