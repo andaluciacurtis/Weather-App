fee5f0c2c5e3787b29b95f016ae9f7d1c23420c3
andaluciacurtis <143487070+andaluciacurtis@users.noreply.github.com>
2023-12-12 19:35:11 +0000

    Messing around with weather API

--- script.js
+++ script.js
@@ -5,6 +5,14 @@
 // Weather necessities:
 // - current weather
 // - weather in an hour, two, three, etc, up to 10
+
+const temperature = document.querySelector('.temperature');
+const weatherDesc = document.querySelector('.weather-desc');
+
+let units = "imperial";
+
+
+
 var cityInput = document.querySelector(".city-input");
 cityInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
@@ -15,8 +23,11 @@ cityInput.addEventListener("keydown", (e) => {
 getWeather();
 
 async function getWeather() {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID=${weatherKey}`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID=${weatherKey}&units=${units}`);
   const data = await response.json();
-  
   console.log(data);
+
+  let mainWeather = data["weather"][0]["main"];
+  temperature.innerHTML = `${Math.round(data["main"]["temp"] * 1)}°F`;
+  weatherDesc.innerHTML = `${mainWeather}`;
 }
